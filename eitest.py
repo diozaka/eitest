0098b909ac6bc28f51d4b86ce35c9ae69ba95865
Erik Scharwächter <scharwaechter@bit.uni-bonn.de>
2020-01-29 17:53:07 +0100

    implemented MMD two-sample test

--- eitest.py
+++ eitest.py
@@ -16,11 +16,14 @@ def distance_transform(event_series):
     return distance_to_event
 
 @njit
-def obtain_samples(event_series, time_series, lag_cutoff=0, instantaneous=False):
+def obtain_samples(event_series, time_series, lag_cutoff=0, instantaneous=True, sort=True):
     '''Compute the samples T_k for all lags k as mentioned in the paper.
 
     Setting lag_cutoff to zero means that all lags are considered, for values larger
-    than zero only lags up to and including the specified value are considered.'''
+    than zero only lags up to and including the specified value are considered.
+    If instantaneous is True, the sample at lag k=0 is included.
+    If sort is True, all samples are sorted ascendingly.'''
+
     dt = distance_transform(event_series)
     min_lag = 0 if instantaneous else 1
     max_lag = max(dt[~np.isinf(dt)])
@@ -30,14 +33,17 @@ def obtain_samples(event_series, time_series, lag_cutoff=0, instantaneous=False)
 
     sample = dict() # dict comprehension not supported by Numba, do it manually
     for lag in range(min_lag, max_lag+1):
-        sample[lag] = np.sort(time_series[dt == lag])
+        samp = time_series[dt == lag]
+        if sort:
+            samp = np.sort(samp)
+        sample[lag] = samp
     return sample
 
 @njit
 def _ks_twosamp_stat(data1, data2, min_pts):
-    '''Numba helper to compute the test statistic value for the Kolmogorov-Smirnov two-sample test.
+    '''Compute the test statistic value for the Kolmogorov-Smirnov two-sample test.
 
-    Adapted from scipy.stats.ks_2samp.'''
+    Adapted from scipy.stats.ks_2samp. data1 and data2 must be sorted.'''
     n1 = len(data1)
     n2 = len(data2)
     if (n1 < min_pts) or (n2 < min_pts):
@@ -51,7 +57,7 @@ def _ks_twosamp_stat(data1, data2, min_pts):
 
 @njit
 def _ks_twosamp_stat_pairwise(sample, min_pts):
-    '''Numba helper to compute all pairwise Kolmogorov-Smirnov two-sample tests.'''
+    '''Compute all pairwise Kolmogorov-Smirnov two-sample tests.'''
     lags = list(sorted(sample.keys()))
     ds  = np.empty(len(lags)*(len(lags)-1)//2)
     ens = np.empty(len(lags)*(len(lags)-1)//2)
@@ -65,6 +71,105 @@ def _ks_twosamp_stat_pairwise(sample, min_pts):
             ens[k] = en
             k += 1
     return ds, ens
+
+@njit
+def _mmd_rbf_dot(s1, s2, deg):
+    '''Compute the radial basis function inner product.
+
+    Adapted for Python from the Matlab MMD implementation by Arthur Gretton:
+    http://www.gatsby.ucl.ac.uk/~gretton/mmd/mmd.htm
+
+    s1, s2: inputs of shape (N, D)
+    deg: kernel size'''
+    G = np.square(s1).sum(axis=1)
+    H = np.square(s2).sum(axis=1)
+    Q = np.repeat(G, s2.shape[0]).reshape(-1, s2.shape[0])
+    R = np.repeat(H, s1.shape[0]).reshape(s1.shape[0],-1).transpose()
+    H = Q + R - 2*np.dot(s1, s2.transpose())
+    H = np.exp(-H/2/deg**2)
+    return H
+
+@njit
+def _mmd_midpoint_heuristic(s1, s2):
+    '''Compute the rbf kernel size from the midpoint heuristic.
+
+    Adapted for Python from the Matlab MMD implementation by Arthur Gretton:
+    http://www.gatsby.ucl.ac.uk/~gretton/mmd/mmd.htm
+
+    s1, s2: inputs of shape (N, D)
+    returns the heuristic value or np.nan, if the heuristic could not be computed'''
+    Z = np.concatenate((s1, s2))
+    N = Z.shape[0]
+    if N > 100:
+        Zmed = Z[:100,:]
+        N = 100
+    else:
+        Zmed = Z
+    G = np.sum(Zmed*Zmed, axis=1)
+    Q = np.repeat(G, N).reshape(N,-1)
+    R = np.repeat(G, N).reshape(N,-1).transpose()
+    dists = Q + R - 2*np.dot(Zmed, Zmed.transpose())
+    dists = (dists - np.tril(dists)).reshape(-1)
+    if np.any(dists > 0):
+        deg = np.sqrt(0.5*np.median(dists[dists > 0]))
+    else:
+        deg = np.nan
+    return deg
+
+@njit
+def _mmd_twosamp_stat(s1, s2, deg):
+    '''Compute the MMD two-sample test statistic and parameters for the Gamma approximation.
+
+    Adapted for Python from the Matlab MMD implementation by Arthur Gretton:
+    http://www.gatsby.ucl.ac.uk/~gretton/mmd/mmd.htm
+
+    s1, s2: inputs of shape (N, D)'''
+    m = s1.shape[0]
+
+    K  = _mmd_rbf_dot(s1, s1, deg)
+    L  = _mmd_rbf_dot(s2, s2, deg)
+    KL = _mmd_rbf_dot(s1, s2, deg)
+
+    tstat  = 1./m**2 * np.sum(K + L - KL - KL.transpose())
+    tstat = tstat * m
+
+    meanMMD = 2./m * (1 - 1./m*np.sum(np.diag(KL)))
+
+    K  = K  - np.diag(np.diag(K))
+    L  = L  - np.diag(np.diag(L))
+    KL = KL - np.diag(np.diag(KL))
+
+    varMMD = 2./m/(m-1) * 1./m/(m-1) * np.sum(np.square(K + L - KL - KL.transpose()))
+
+    gamma_shape = meanMMD**2 / varMMD
+    gamma_scale = varMMD*m / meanMMD
+
+    return tstat, gamma_shape, gamma_scale
+
+@njit
+def _mmd_twosamp_stat_pairwise(sample, min_pts):
+    '''Numba helper to compute all pairwise MMD two-sample tests.'''
+    lags = list(sorted(sample.keys()))
+    tstats = np.empty(len(lags)*(len(lags)-1)//2)
+    g_shps = np.empty(len(lags)*(len(lags)-1)//2)
+    g_scls = np.empty(len(lags)*(len(lags)-1)//2)
+    k = 0
+    for i in range(len(lags)):
+        data1 = sample[lags[i]].reshape(len(sample[lags[i]]), -1) # cast shape T to Tx1, if necessary
+        for j in range(i+1, len(lags)):
+            data2 = sample[lags[j]].reshape(len(sample[lags[j]]), -1)
+
+            # samples have to be cropped to the same length
+            m = min(len(data1), len(data2))
+            if m < min_pts:
+                tstats[k], g_shps[k], g_scls[k] = np.nan, np.nan, np.nan
+            else:
+                data1c = data1[:m,:]
+                data2c = data2[:m,:]
+                deg = _mmd_midpoint_heuristic(data1c, data2c)
+                tstats[k], g_shps[k], g_scls[k] = _mmd_twosamp_stat(data1c, data2c, deg)
+            k += 1
+    return tstats, g_shps, g_scls
 
 def pairwise_twosample_tests(sample, test, min_pts=2):
     '''For each pair of lags i<j, test whether the distributions at lags i and j are identical.
@@ -85,8 +190,12 @@ def pairwise_twosample_tests(sample, test, min_pts=2):
         # SciPy 1.4.x (with mode='exact'):
         # TODO
         return ds, probs
+    elif test == 'mmd':
+        tstats, g_shps, g_scls = _mmd_twosamp_stat_pairwise(sample, min_pts)
+        probs = np.array([ss.distributions.gamma.sf(tstats[k], g_shps[k], scale=g_scls[k]) for k in range(len(tstats))])
+        return tstats, probs
     else:
-        raise NotImplementedError("only ks implemented so far")
+        raise NotImplementedError('test must be either ks or mmd')
 
 def multitest(sorted_pvals, adjust):
     adj_pvals = np.empty_like(sorted_pvals)
